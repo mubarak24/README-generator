692cab4cac798300e942d9365832bac7fe14eda8
mubarak <mubarakcali24@gmail.com>
2023-04-22 14:35:34 -0700

    writetofile

--- index.js
+++ index.js
@@ -2,61 +2,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'project',
-        message: 'What is your project name',
-    },
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Project',
+            message: 'What is your project name',
+        },
 
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'README description',
-    },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'README description',
+        },
 
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'installation instructions',
-    },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'installation instructions',
+        },
 
-    {
-        type: 'input',
-        name: 'Usage',
-        message: 'usage information',
-    },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'usage information',
+        },
 
-    {
-        type: 'input',
-        name: 'Contributing',
-        message: 'contribution guidelines',
-    },
+        {
+            type: 'input',
+            name: 'Contributing',
+            message: 'contribution guidelines',
+        },
 
-    {
-        type: 'input',
-        name: 'Tests',
-        message: 'test instructions',
-    },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'test instructions',
+        },
 
-    {
-        type: 'input',
-        name: 'Github',
-        message: 'What is your Github username?',
-    },
+        {
+            type: 'input',
+            name: 'Github',
+            message: 'What is your Github username?',
+        },
 
-    {
-        type: 'input',
-        name: 'Email',
-        message: 'What is your email address?',
-    },
-];
-    
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is your email address?',
+        },
+    ]);
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    Name
+    ${Project}
+    Description
+    ${Description, Github, Email}
+    Badges
+    ${}
+    Tests
+    ${Tests}
+    Installation
+    ${Installation}
+    Usage
+    ${Usage}
+    Contributing
+    ${Contributing}
+    License
+    ${}
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
