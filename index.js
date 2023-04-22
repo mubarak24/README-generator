02c1a18177669f9564b7e5e16810eeb60d60994d
mubarak <mubarakcali24@gmail.com>
2023-04-22 13:53:46 -0700

    array of questions

--- index.js
+++ index.js
@@ -2,8 +2,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project name',
+    },
 
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'README description',
+    },
+
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'installation instructions',
+    },
+
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'usage information',
+    },
+
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'contribution guidelines',
+    },
+
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'test instructions',
+    },
+
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your Github username?',
+    },
+
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email address?',
+    },
+];
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
